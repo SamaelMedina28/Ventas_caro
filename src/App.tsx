0d3a521d4836e0c647408ba32f88ee800a28e305
Samael Home <samaelortiz2218@gmail.com>
2025-08-12 20:17:04 -0700

    Seccion de pregunats frecuentes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import NavHeader from "./components/sections/Navbar";
 import Productos from "./components/sections/Productos";
 import CallToAction from "./components/CallToAction";
 import Servicios from "./components/sections/Servicios";
+import Preguntas from "./components/sections/Preguntas";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       <Productos/>
       <Servicios/>
       <CallToAction/>
+      <Preguntas/>
     </div>
   )
 }
